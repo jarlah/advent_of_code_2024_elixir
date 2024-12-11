1803c276fc90e86fbe5faef81caf8a133c07467b
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-11 22:33:47 +0100

    2015 day1 part2 done

--- lib/2015/day_1/Part2.ex
+++ lib/2015/day_1/Part2.ex
@@ -3,8 +3,25 @@ defmodule AOC2015.Day1.Part2.Solution do
   ## Examples
 
       iex> AOC2015.Day1.Part2.Solution.solution(AOC2015.Day1.Input.input())
-      0
+      {-1, 1771}
 
   """
-  def solution(_input), do: 0
+  def solution(input) do
+    input
+    |> String.to_charlist()
+    |> Enum.map(&<<&1>>)
+    |> Enum.reduce_while({0, 0}, fn paren, {floor, count} ->
+      if floor == -1 do
+        {:halt, {floor, count}}
+      else
+        case paren do
+          "(" ->
+            {:cont, {floor + 1, count + 1}}
+
+          ")" ->
+            {:cont, {floor - 1, count + 1}}
+        end
+      end
+    end)
+  end
 end
