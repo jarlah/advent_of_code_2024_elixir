9026e6737c0622ece19c65fc87e95db17213b322
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-11 10:53:15 +0100

    refactor

--- lib/2024/day_11/Part1.ex
+++ lib/2024/day_11/Part1.ex
@@ -8,50 +8,40 @@ defmodule AOC2024.Day11.Part1.Solution do
       185205
   """
   def solution(input, repeats) do
-    memo = %{}
-
-    stones =
-      input
-      |> String.split(" ")
-      |> Enum.map(&String.to_integer/1)
-
-    total_stones = length(stones)
-
-    stones
-    |> Enum.reduce({0, memo}, fn stone, {sum, memo} ->
-      {count, new_memo} = count_after_transforms(stone, repeats, memo, 1, total_stones)
+    input
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce({0, %{}}, fn stone, {sum, memo} ->
+      {count, new_memo} = count_after_transforms(stone, repeats, memo)
       {sum + count, new_memo}
     end)
     |> elem(0)
   end
 
-  defp count_after_transforms(stone, repeats, memo, current_stone, total_stones) do
-    case Map.get(memo, {stone, repeats}) do
+  defp count_after_transforms(stone, repeats, memo) do
+    case memo |> Map.get({stone, repeats}) do
       nil ->
-        # IO.puts("Processing stone #{current_stone}/#{total_stones}: #{stone} (#{repeats} repeats left)")
         {count, new_memo} =
-          do_count_after_transforms(stone, repeats, memo, current_stone, total_stones)
+          do_count_after_transforms(stone, repeats, memo)
 
-        {count, Map.put(new_memo, {stone, repeats}, count)}
+        {count, new_memo |> Map.put({stone, repeats}, count)}
 
       count ->
-        # IO.puts("Cache hit for stone #{current_stone}/#{total_stones}: #{stone} (#{repeats} repeats left)")
         {count, memo}
     end
   end
 
-  defp do_count_after_transforms(_stone, 0, memo, _current_stone, _total_stones), do: {1, memo}
+  defp do_count_after_transforms(_stone, 0, memo), do: {1, memo}
 
-  defp do_count_after_transforms(stone, repeats, memo, current_stone, total_stones) do
+  defp do_count_after_transforms(stone, repeats, memo) do
     {transformed, new_memo} = transform_stone_memo(stone, memo)
 
     {counts, final_memo} =
-      transformed
-      |> Enum.map_reduce(new_memo, fn s, m ->
-        count_after_transforms(s, repeats - 1, m, current_stone, total_stones)
+      Enum.map_reduce(transformed, new_memo, fn s, m ->
+        count_after_transforms(s, repeats - 1, m)
       end)
 
-    {counts |> Enum.sum(), final_memo}
+    {Enum.sum(counts), final_memo}
   end
 
   defp transform_stone_memo(stone, memo) do
