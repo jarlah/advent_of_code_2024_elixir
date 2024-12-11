3a8753cbdadf3127cba796b58c6e12448dc0a5aa
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-11 18:35:56 +0100

    logs

--- lib/2024/day_11/Part1.ex
+++ lib/2024/day_11/Part1.ex
@@ -2,20 +2,26 @@ defmodule AOC2024.Day11.Part1.Solution do
   @doc ~S"""
   ## Examples
 
-      iex> AOC2024.Day11.Part1.Solution.solution(AOC2024.Day11.Input.test_input(), 6)
+      iex> AOC2024.Day11.Part1.Solution.solution(AOC2024.Day11.Input.test_input(), 6, true)
       22
       iex> AOC2024.Day11.Part1.Solution.solution(AOC2024.Day11.Input.input(), 25)
       185205
   """
-  def solution(input, repeats) do
+  def solution(input, repeats, log \\ false) do
     input
     |> String.split(" ")
     |> Enum.map(&String.to_integer/1)
     |> Enum.reduce({0, %{}}, fn stone, {sum, memo} ->
-      calculate_stone_count(stone, repeats, memo)
+      calculate_stone_count(stone, repeats, memo, log)
       |> then(
         &{
-          sum + elem(&1, 0),
+          sum +
+            (elem(&1, 0)
+             |> tap(
+               if log,
+                 do: fn sum -> IO.inspect(sum, label: "total count for stone #{stone}") end,
+                 else: fn _ -> nil end
+             )),
           elem(&1, 1)
         }
       )
@@ -23,14 +29,19 @@ defmodule AOC2024.Day11.Part1.Solution do
     |> elem(0)
   end
 
-  defp calculate_stone_count(stone, repeats, memo) do
-    case memo |> Map.get({stone, repeats}) do
+  defp calculate_stone_count(stone, repeats, memo, log) do
+    case memo |> Map.get({:repeats, repeats, stone}) do
       nil ->
-        process_stone_transformations(stone, repeats, memo)
+        process_stone_transformations(stone, repeats, memo, log)
         |> then(
           &{
-            elem(&1, 0),
-            elem(&1, 1) |> Map.put({stone, repeats}, elem(&1, 0))
+            elem(&1, 0)
+            |> tap(
+              if log,
+                do: fn sum -> IO.inspect(sum, label: "intermediate count for #{stone}") end,
+                else: fn _ -> nil end
+            ),
+            elem(&1, 1) |> Map.put({:repeats, repeats, stone}, elem(&1, 0))
           }
         )
 
@@ -39,14 +50,16 @@ defmodule AOC2024.Day11.Part1.Solution do
     end
   end
 
-  defp process_stone_transformations(_stone, 0, memo), do: {1, memo}
+  defp process_stone_transformations(_stone, 0, memo, _log), do: {1, memo}
 
-  defp process_stone_transformations(stone, repeats, memo) do
-    {transformed, new_memo} = transform_stone_memo(stone, memo)
+  defp process_stone_transformations(stone, repeats, memo, log) do
+    {transformed, new_memo} = transform_stone_with_memo(stone, memo)
 
-    Enum.map_reduce(transformed, new_memo, fn s, m ->
-      calculate_stone_count(s, repeats - 1, m)
-    end)
+    Enum.map_reduce(
+      transformed,
+      new_memo,
+      &calculate_stone_count(&1, repeats - 1, &2, log)
+    )
     |> then(
       &{
         Enum.sum(elem(&1, 0)),
@@ -55,14 +68,14 @@ defmodule AOC2024.Day11.Part1.Solution do
     )
   end
 
-  defp transform_stone_memo(stone, memo) do
-    case memo |> Map.get({:transform, stone}) do
+  defp transform_stone_with_memo(stone, memo) do
+    case memo |> Map.get({:stones, stone}) do
       nil ->
-        result = transform_stone(stone)
-        {result, memo |> Map.put({:transform, stone}, result)}
+        stones = transform_stone(stone)
+        {stones, memo |> Map.put({:stones, stone}, stones)}
 
-      result ->
-        {result, memo}
+      stones ->
+        {stones, memo}
     end
   end
 
