8138b702d8748da5ac67f997a615720b40330a2a
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-11 11:19:15 +0100

    prep day12

---
+++ lib/2024/day_12/Part2.ex
@@ -0,0 +1,10 @@
+defmodule AOC2024.Day12.Part2.Solution do
+  @doc ~S"""
+  ## Examples
+
+      iex> AOC2024.Day12.Part2.Solution.solution(AOC2024.Day12.Input.input())
+      0
+
+  """
+  def solution(_input), do: 0
+end
