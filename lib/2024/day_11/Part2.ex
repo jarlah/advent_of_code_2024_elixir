7805b70c8dcd73986ea6bb20e3ab431f823d735c
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-11 12:07:59 +0100

    allow overriding num

--- lib/2024/day_11/Part2.ex
+++ lib/2024/day_11/Part2.ex
@@ -2,9 +2,11 @@ defmodule AOC2024.Day11.Part2.Solution do
   @doc ~S"""
   ## Examples
 
-      iex> AOC2024.Day11.Part2.Solution.solution(AOC2024.Day11.Input.input())
+      iex> AOC2024.Day11.Part2.Solution.solution(AOC2024.Day11.Input.input(), 75)
       221280540398419
 
   """
-  def solution(input), do: AOC2024.Day11.Part1.Solution.solution(input, 75)
+  def solution(input, num) do
+    AOC2024.Day11.Part1.Solution.solution(input, num)
+  end
 end
