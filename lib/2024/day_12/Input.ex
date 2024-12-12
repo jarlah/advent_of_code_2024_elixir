f5a5a15930c4d1a86a6f6b8579792615684ea09c
Jarl André Hübenthal <jarlah@protonmail.com>
2024-12-12 07:05:31 +0100

    add tests for day 12

--- lib/2024/day_12/Input.ex
+++ lib/2024/day_12/Input.ex
@@ -5,4 +5,38 @@ defmodule AOC2024.Day12.Input do
     |> Enum.map(&String.trim/1)
     |> Enum.reject(&(&1 == ""))
   end
+
+  def test_input do
+    [
+      "RRRRIICCFF",
+      "RRRRIICCCF",
+      "VVRRRCCFFF",
+      "VVRCCCJFFF",
+      "VVVVCJJCFE",
+      "VVIVCCJJEE",
+      "VVIIICJJEE",
+      "MIIIIIJJEE",
+      "MIIISIJEEE",
+      "MMMISSJEEE"
+    ]
+  end
+
+  def test_input_1 do
+    [
+      "AAAA",
+      "BBCD",
+      "BBCC",
+      "EEEC"
+    ]
+  end
+
+  def test_input_2 do
+    [
+      "OOOOO",
+      "OXOXO",
+      "OOOOO",
+      "OXOXO",
+      "OOOOO"
+    ]
+  end
 end
--- lib/2024/day_12/Part1.ex
+++ lib/2024/day_12/Part1.ex
@@ -2,6 +2,12 @@ defmodule AOC2024.Day12.Part1.Solution do
   @doc ~S"""
   ## Examples
 
+      iex> AOC2024.Day12.Part1.Solution.solution(AOC2024.Day12.Input.test_input_1())
+      140
+      iex> AOC2024.Day12.Part1.Solution.solution(AOC2024.Day12.Input.test_input_2())
+      772
+      iex> AOC2024.Day12.Part1.Solution.solution(AOC2024.Day12.Input.test_input())
+      1930
       iex> AOC2024.Day12.Part1.Solution.solution(AOC2024.Day12.Input.input())
       0
 
